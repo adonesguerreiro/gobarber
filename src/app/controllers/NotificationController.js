95bea272caa3d4f7cf6d4e3065ef4adb6308b829
root <adoneslori@gmail.com>
2020-07-13 20:55:31 -0400

    adicionando arquivos

---
+++ src/app/controllers/NotificationController.js
@@ -0,0 +1,33 @@
+import User from '../models/User';
+import Notification from '../schemas/Notifications';
+
+class NotificationController {
+  async index(req, res) {
+    const checkIsProvider = User.findOne({
+      where: { provider_id: req.userId, provider: true },
+    });
+
+    if (!checkIsProvider) {
+      res.status(401).json({ error: 'Appointment date is not available' });
+    }
+
+    const notifications = await Notification.find({
+      user: req.userId,
+    })
+      .sort({ createdAt: 'desc' })
+      .limit(20);
+
+    return res.json(notifications);
+  }
+
+  async update(req, res) {
+    const notification = await Notification.findByIdAndUpdate(
+      req.params.id,
+      { read: true },
+      { new: true }
+    );
+
+    return res.json(notification);
+  }
+}
+export default new NotificationController();
